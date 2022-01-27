d20a00291a28175c705097aacada4ba7399272f7
akash19here <spacex3nasa@gmail.com>
2022-01-27 15:19:28 +0530

    Broken homepage into smaller components,finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <HomePage/>
   );
-}
+} 
 
 export default App;
